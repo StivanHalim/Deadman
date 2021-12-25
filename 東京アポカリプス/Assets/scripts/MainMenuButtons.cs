caca343ff4456ff0032be65dedf3cbe04b038618
Stivan Effendi Halim <stivanhalim@gmail.com>
2021-12-25 19:47:12 +0900

    Zombie Game
    詳細はAssetsの中にあります

---
+++ 東京アポカリプス/Assets/scripts/MainMenuButtons.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainMenuButtons : MonoBehaviour {
+
+    public SpriteRenderer Background;
+    public SpriteRenderer Background1;
+    bool on = false;
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void BackgroundActivate()
+    {
+        if (on == true)
+        {
+            on = false;
+        }
+        else
+        {
+            on = true;
+        }
+
+        if (on == true)
+        {
+            Background.enabled = false;
+            Background1.enabled = true;
+        }
+        else
+        {
+            Background.enabled = true;
+            Background1.enabled = false;
+        }
+    }
+}
